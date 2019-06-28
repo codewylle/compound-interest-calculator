4cd8912c807028e9471902e8f94ca4270e48bbc3
Danila Sukhanov <codewylle@gmail.com>
2019-06-28 14:20:40 +0500

    Add calculation of simple interest

--- src/components/Calc/Calc.tsx
+++ src/components/Calc/Calc.tsx
@@ -5,14 +5,27 @@ import { observer } from "mobx-react-lite";
 import "./Calc.css";
 import { CalcModel } from "../../model/CalcModel";
 
-function _Calc(
-	{
-		state,
-	}: {
-		state: Instance<typeof CalcModel>,
-	}
-): JSX.Element | null {
-	return null;
+import { InputNumber } from "../InputNumber/InputNumber";
+import { InputStateless } from "../InputStateless/InputStateless";
+
+function _Calc({
+	state,
+}: {
+	state: Instance<typeof CalcModel>,
+}): JSX.Element | null {
+	return (
+		<form className="calc" onSubmit={e => e.preventDefault()}>
+			<h1 className="calc__header">Кредитный калькулятор</h1>
+			<InputNumber label="Тело кредита" state={state.primaryLoan} />
+			<InputNumber label="Ежемесячный платёж" state={state.monthlyPayment} />
+			<InputNumber label="Срок в месяцах" state={state.numberOfMonths} />
+			<br />
+			<InputStateless
+				label="Простой процент" input={state.yearlySimpleInterestFormatted} />
+			<InputStateless
+				label="Сложный процент" input={state.yearlyCompoundInterestFormatted} />
+		</form>
+	)
 }
 
 export const Calc = observer(_Calc);
--- src/model/CalcModel.ts
+++ src/model/CalcModel.ts
@@ -1,4 +1,54 @@
 import { types } from "mobx-state-tree";
 
+import { InputNumberModel } from "./InputNumberModel";
+
+function formatInterest(value: number, info?: string): string {
+    if (isNaN(value)) {
+        return "НЕДОСТАТОЧНО ДАННЫХ";
+    }
+
+    const body = `${((value - 1) * 100).toFixed(2)}%`;
+
+    return info ? `${body} ${info}` : body;
+}
+
 export const CalcModel = types.model("Calc", {
-});
+    primaryLoan: types.optional(InputNumberModel, {}),
+    monthlyPayment: types.optional(InputNumberModel, {}),
+    numberOfMonths: types.optional(InputNumberModel, {}),
+})
+    .views(self => ({
+        get notEnoughDataToCalcInterest(): boolean {
+            return self.primaryLoan.empty || !self.primaryLoan.valid
+                || self.monthlyPayment.empty || !self.monthlyPayment.valid
+                || self.numberOfMonths.empty || !self.numberOfMonths.valid;
+        }
+    }))
+    .views(self => ({
+        get monthlySimpleInterest(): number {
+            if (self.notEnoughDataToCalcInterest) {
+                return NaN;
+            }
+
+            return self.monthlyPayment.value / self.primaryLoan.value;
+        },
+    }))
+    .views(self => ({
+        get yearlySimpleInterest(): number {
+            return self.monthlySimpleInterest * Math.min(self.numberOfMonths.value, 12);
+        },
+    }))
+    .views(self => ({
+        get monthlySimpleInterestFormatted(): string {
+            return formatInterest(self.monthlySimpleInterest, "от тела кредита в месяц");
+        },
+        get yearlySimpleInterestFormatted(): string {
+            return formatInterest(self.yearlySimpleInterest, "от тела кредита в год");
+        },
+        get monthlyCompoundInterestFormatted(): string {
+            return "НЕ РЕАЛИЗОВАНО";
+        },
+        get yearlyCompoundInterestFormatted(): string {
+            return "НЕ РЕАЛИЗОВАНО";
+        }
+    }));
